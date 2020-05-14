d17e073c167b063d082d5f0739dc4a685f8174cf
exiou <ryan.oliveira23@etec.sp.gov.br>
2020-05-13 21:45:55 -0300

    Metade da aplicação

---
+++ App_BD_SQLite/App_BD_SQLite/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using App_BD_SQLite.View;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App_BD_SQLite
+{
+    public partial class App : Application
+    {
+
+        public static String DbName;
+        public static String DbPath;
+        public App(string dbPath, string dbName)
+        {
+            InitializeComponent();
+
+            App.DbName = dbName;
+            App.DbPath = dbPath;
+            MainPage = new PageMenu();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ App_BD_SQLite/App_BD_SQLite/View/PageMenu.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App_BD_SQLite.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PageMenu : MasterDetailPage
+    {
+        public PageMenu()
+        {
+            InitializeComponent();
+            Detail = new NavigationPage((Page)Activator.CreateInstance(typeof(HomeDetail)));
+        }
+
+        private async void Open_Home(object sender, EventArgs e)
+        {
+            try
+            {
+                Detail = new NavigationPage((Page)Activator.CreateInstance(typeof(HomeDetail)));
+                IsPresented = false;
+            }catch(Exception ex)
+            {
+                await DisplayAlert("Ops!", ex.Message, "OK");
+            }
+        }
+
+        private async void Open_Cadastrar(object sender, EventArgs e)
+        {
+            try
+            {
+                Detail = new NavigationPage((Page)Activator.CreateInstance(typeof(CadastrarDetail)));
+                IsPresented = false;
+            }catch(Exception ex)
+            {
+                await DisplayAlert("Ops!", ex.Message, "OK");
+            }
+        }
+
+        private async void Open_Listar(object sender, EventArgs e)
+        {
+            try
+            {
+                Detail = new NavigationPage((Page)Activator.CreateInstance(typeof(ListarDetail)));
+                IsPresented = false;
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Ops!", ex.Message, "OK");
+            }
+        }
+    }
+}
